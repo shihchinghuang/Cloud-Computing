99d7e6e40040bc330b88a82c127cb06ce456427c
Sunny Huang <82016503+shihchinghuang@users.noreply.github.com>
2022-12-08 21:08:01 -0800

    Create sparkProcessor.py

---
+++ Apache Kafka + Spark Streaming + PySpark/sparkProcessor.py
@@ -0,0 +1,39 @@
+import sys
+from pyspark import SparkConf, SparkContext
+from pyspark.streaming import StreamingContext
+
+#from pyspark.streaming.kafka import KafkaUtils
+
+
+def process_events(event):
+    return (event[0], Counter(event[1].split(" ")).most_common(3))
+
+
+def push_back_to_kafka(processed_events):
+    list_of_processed_events = processed_events.collect()
+    producer.send('output_event', value = str(list_of_processed_events))
+
+
+def spark_context_creator():
+    conf = SparkConf()
+    #The master URL to connect and set name for our app
+    conf.setMaster("spark://34.70.211.224:7077").setAppName("ConnectingDotsSparkKafkaStreaming")
+    sc = None
+    try:
+        sc.stop()
+        sc = SparkContext(conf=conf)
+    except:
+        sc = SparkContext(conf=conf)
+    return sc
+
+
+if __name__ == "__main__":
+    sc = spark_context_creator()
+    ssc = StreamingContext(sc, 1)
+
+    kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', 'test-consumer-group', {'input_event':1})
+    lines = kafkaStream.map(lambda x : process_events(x))
+
+    producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=str.encode, key_serializer=str.encode)
+
+    lines.foreachRDD(push_back_to_kafka)
